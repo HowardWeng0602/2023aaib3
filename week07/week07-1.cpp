875810e89cafcccd0d86772a571b8e11a8df3788
HowardWeng0602 <145088372+HowardWeng0602@users.noreply.github.com>
2024-10-23 11:54:27 +0800

    week07

---
+++ week07/week07-1.cpp
@@ -0,0 +1,26 @@
+//week07-1.cpp
+//Leetcode 1275
+class Solution {
+public:
+    void myDrawBoard(int board[3][3]) {
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                cout << board[i][j] << " ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+    string tictactoe(vector<vector<int>>& moves) {
+        int board[3][3] = {};
+        int now = 1; //1,2,1,2
+        myDrawBoard(board);
+        for(auto move : moves){ // Step01: C++°j°é
+            int i = move[0],j = move[1];
+            board[i][j] = 1;
+            myDrawBoard(board);
+
+        }
+        return "B";// "A" or "B" or "Draw"¥­¤â
+    }
+};
