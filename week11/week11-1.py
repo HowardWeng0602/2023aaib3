59728228a2841295bb26cbf2371919997a13ad87
HowardWeng0602 <145088372+HowardWeng0602@users.noreply.github.com>
2024-11-20 11:54:19 +0800

    week11

---
+++ week11/week11-1.py
@@ -0,0 +1,10 @@
+# 開心的 +程式碼 Python
+# week11-1.py 主題 Hash Map, Hash Set
+table = {}
+table[9977341] = 9
+table[4433997] = 4
+
+print(table[9977341]) # 會印出 9
+print(table[4433997]) # 會印出 4
+if 0 not in table:
+  print("table[0]不存在")
